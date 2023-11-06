b4867b070c46250a52ad039489a0fbc7c6f67e56
YO-U <enoxy7@gmail.com>
2023-11-07 01:38:55 +0200

    List

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace MyApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = new List<int>(10);
+
+            numbers.Add(5);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+            numbers.Add(2);
+
+            numbers.AddRange(new int[] { 1, 2, 3, 4 });
+
+            numbers.RemoveAt(5);//удаление по индексу
+            numbers.Remove(4);//удалить первую встречну 4
+            numbers.IndexOf(5);//получить индекс позиции числа 5
+            numbers.Insert(0, 2); //добавить элемент на 0 индекс
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
         }
     }
 }
