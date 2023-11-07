64b6c586f7ff43a300e7e09af5f1a5c4e6ad0a8d
YO-U <enoxy7@gmail.com>
2023-11-08 01:11:15 +0200

    Queue

--- Program.cs
+++ Program.cs
@@ -6,26 +6,20 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>(10);
+            Queue<string> queue = new Queue<string>();// FIFO first in first out первый зашел первый вышел
 
-            numbers.Add(5);
-            numbers.Add(2);
-            numbers.Add(3);
-            numbers.Add(4);
-            numbers.Add(5);
-            numbers.Add(2);
+            queue.Enqueue("Vasa");//добавить элемент
+            queue.Enqueue("Roma");
+            queue.Enqueue("Goga");
+            queue.Enqueue("Popka");
 
-            numbers.AddRange(new int[] { 1, 2, 3, 4 });
-
-            numbers.RemoveAt(5);//удаление по индексу
-            numbers.Remove(4);//удалить первую встречну 4
-            numbers.IndexOf(5);//получить индекс позиции числа 5
-            numbers.Insert(0, 2); //добавить элемент на 0 индекс
-
-            for (int i = 0; i < numbers.Count; i++)
+            foreach (var item in queue)
             {
-                Console.WriteLine(numbers[i]);
+                Console.WriteLine(item);
             }
+            Console.WriteLine(queue.Dequeue());//использует первый по списку элемент и убирает его
+            Console.WriteLine(queue.Peek());//показывает какой элемент будет после первого
+
         }
     }
 }
